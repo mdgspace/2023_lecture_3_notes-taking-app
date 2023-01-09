5fb8935776882bf2c5bd1b658cb2b813c9959832
nik-55 <nik.xyz.in@gmail.com>
2023-01-10 02:40:37 +0530

    feat(home): Create Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import "./styles/global.css"
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import mdg_logo from "../assests/images/mdg_logo.svg";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-white p-3 ">
+      <img alt="mdg-logo" src={mdg_logo} />
+      <div>
+        username
+        <img
+          alt="profile-avatar"
+          src={"https://api.multiavatar.com/kathrin.svg"}
+          className="profile-avatar"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
